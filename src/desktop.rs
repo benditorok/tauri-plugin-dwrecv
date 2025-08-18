6f8fa755b193b0c687c9d5c0822de2f8b71dd817
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2025-08-18 16:42:20 +0200

    Init

---
+++ src/desktop.rs
@@ -0,0 +1,22 @@
+use serde::de::DeserializeOwned;
+use tauri::{plugin::PluginApi, AppHandle, Runtime};
+
+use crate::models::*;
+
+pub fn init<R: Runtime, C: DeserializeOwned>(
+  app: &AppHandle<R>,
+  _api: PluginApi<R, C>,
+) -> crate::Result<Dwrecv<R>> {
+  Ok(Dwrecv(app.clone()))
+}
+
+/// Access to the dwrecv APIs.
+pub struct Dwrecv<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Dwrecv<R> {
+  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    Ok(PingResponse {
+      value: payload.value,
+    })
+  }
+}
---
+++ src/lib.rs
@@ -0,0 +1,48 @@
+use tauri::{
+  plugin::{Builder, TauriPlugin},
+  Manager, Runtime,
+};
+
+pub use models::*;
+
+#[cfg(desktop)]
+mod desktop;
+#[cfg(mobile)]
+mod mobile;
+
+mod commands;
+mod error;
+mod models;
+
+pub use error::{Error, Result};
+
+#[cfg(desktop)]
+use desktop::Dwrecv;
+#[cfg(mobile)]
+use mobile::Dwrecv;
+
+/// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the dwrecv APIs.
+pub trait DwrecvExt<R: Runtime> {
+  fn dwrecv(&self) -> &Dwrecv<R>;
+}
+
+impl<R: Runtime, T: Manager<R>> crate::DwrecvExt<R> for T {
+  fn dwrecv(&self) -> &Dwrecv<R> {
+    self.state::<Dwrecv<R>>().inner()
+  }
+}
+
+/// Initializes the plugin.
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+  Builder::new("dwrecv")
+    .invoke_handler(tauri::generate_handler![commands::ping])
+    .setup(|app, api| {
+      #[cfg(mobile)]
+      let dwrecv = mobile::init(app, api)?;
+      #[cfg(desktop)]
+      let dwrecv = desktop::init(app, api)?;
+      app.manage(dwrecv);
+      Ok(())
+    })
+    .build()
+}
