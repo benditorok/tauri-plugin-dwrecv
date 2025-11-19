a8e8f10b0a4fee6346e2ab8f1c40ded37eca76fa
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2025-11-19 08:49:31 +0100

    Update comments

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
-/// https://github.com/tauri-apps/tauri/issues/13027
-///
-/// API changed in 2.9.x
-/// https://github.com/tauri-apps/tauri/issues/13027#issuecomment-3457217759
-/// https://github.com/tauri-apps/tauri/pull/14132
-///
-/// RegisterListener
-/// * Source: https://github.com/tauri-apps/tauri/blob/dev/packages/api/src/core.ts#L189
+/// Commands:
+/// * `register_listener` is required so that the user can subscribe to the plugin's window events.
+///   * Additional information:
+///     * Source: https://github.com/tauri-apps/tauri/blob/dev/packages/api/src/core.ts#L189
+///     * https://github.com/tauri-apps/tauri/issues/13027
+///     * API changed in 2.9.x
+///     * https://github.com/tauri-apps/tauri/issues/13027#issuecomment-3457217759
+///     * https://github.com/tauri-apps/tauri/pull/14132
 const COMMANDS: &[&str] = &["ping", "register_listener"];
 
 fn main() {
